4495560cb2e5b6428aef2fb11670d5c9bf2ba0f8
Mehmetali ÖZTÜRK <mehmetali.ozturk@hotmail.com>
2025-02-22 19:19:35 +0300

    Personel sayfası yeni altyapıya entegre edildi.

--- app/personel/page.tsx
+++ app/personel/page.tsx
@@ -1,6 +1,7 @@
-'use client'
-import { useState, useEffect } from 'react';
-import Image from 'next/image';
+"use client";
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import Loading from "../loading";
 
 interface Personnel {
   id: number;
@@ -14,31 +15,34 @@ export default function Personnel() {
   const [showVideo, setShowVideo] = useState(false);
   const [personnel, setPersonnel] = useState<Personnel[]>([]);
   const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(true); // 3 saniyelik yükleme için state
 
   useEffect(() => {
+    setTimeout(() => setIsLoading(false), 1000); // 3 saniyelik gecikmeli yükleme
+
     fetchPersonnel();
   }, []);
 
   const fetchPersonnel = async () => {
     try {
-      const response = await fetch('/api/personnel');
+      const response = await fetch("/api/personnel");
       if (!response.ok) {
-        throw new Error('Veri çekme hatası');
+        throw new Error("Veri çekme hatası");
       }
       const data = await response.json();
       setPersonnel(data);
     } catch (err) {
-      setError(err instanceof Error ? err.message : 'Bir hata oluştu');
-      console.error('Error fetching personnel:', err);
+      setError(err instanceof Error ? err.message : "Bir hata oluştu");
+      console.error("Error fetching personnel:", err);
     }
   };
+
   useEffect(() => {
     const video = document.getElementById("bragovich-video") as HTMLVideoElement;
     if (showVideo && video) {
-      video.currentTime = 0; // Videoyu başa sar
-      video.play().catch(err => console.error("Video oynatma hatası:", err));
-      
-      // Video bittiğinde state'i sıfırla
+      video.currentTime = 0;
+      video.play().catch((err) => console.error("Video oynatma hatası:", err));
+
       video.onended = () => {
         setShowVideo(false);
       };
@@ -56,14 +60,21 @@ export default function Personnel() {
     return rank;
   };
 
-  const groupedPersonnel = personnel.reduce<Record<string, typeof personnel>>((acc, person) => {
-    const normalizedRank = normalizeRank(person.rank);
-    if (!acc[normalizedRank]) {
-      acc[normalizedRank] = [];
-    }
-    acc[normalizedRank].push(person);
-    return acc;
-  }, {});
+  const groupedPersonnel = personnel.reduce<Record<string, typeof personnel>>(
+    (acc, person) => {
+      const normalizedRank = normalizeRank(person.rank);
+      if (!acc[normalizedRank]) {
+        acc[normalizedRank] = [];
+      }
+      acc[normalizedRank].push(person);
+      return acc;
+    },
+    {}
+  );
+
+  if (isLoading) {
+    return <Loading />;
+  }
 
   if (error) {
     return <div className="text-red-500 text-center p-4">{error}</div>;
@@ -71,12 +82,17 @@ export default function Personnel() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-white">
-      <div className="min-w-full text-center mb-8 bg-lapd-accent-light/50 py-10">
+      <div className="min-w-full text-center mb-8 bg-white py-10">
         <h1 className="text-4xl font-bold text-lapd-primary mb-4">Personel</h1>
-        <p className="text-lapd-primary-dark">Şehrimizin güvenliği için çalışan personellerimiz.</p>
+        <p className="text-lapd-primary-dark">
+          Şehrimizin güvenliği için çalışan personellerimiz.
+        </p>
       </div>
       {Object.keys(groupedPersonnel).map((rank, index) => (
-        <div key={index} className="w-full max-w-4xl mb-8 flex flex-col items-center">
+        <div
+          key={index}
+          className="w-full max-w-4xl mb-8 flex flex-col items-center"
+        >
           <h2 className="text-2xl font-semibold text-lapd-primary mb-4 text-center">
             {rank}
           </h2>
@@ -85,7 +101,9 @@ export default function Personnel() {
               <div
                 key={idx}
                 className="flex flex-col items-center bg-white p-4 rounded-lg w-64 h-max"
-                onClick={() => person.name === "BragOvich" ? playVideo() : null}
+                onClick={() =>
+                  person.name === "BragOvich" ? playVideo() : null
+                }
               >
                 {person.name === "BragOvich" && showVideo ? (
                   <div className="w-full h-[220px] relative">
@@ -98,7 +116,7 @@ export default function Personnel() {
                     />
                   </div>
                 ) : (
-                  <div className="relative w-full h-[220px] mb-4">
+                  <div className="relative w-full h-[224px] mb-4">
                     <Image
                       src={person.img_src}
                       alt={person.name}
@@ -108,13 +126,17 @@ export default function Personnel() {
                     />
                   </div>
                 )}
-                <h3 className="text-xl font-semibold text-lapd-primary text-center">{person.name}</h3>
+                <h3 className="text-xl font-semibold text-lapd-primary text-center">
+                  {person.name}
+                </h3>
                 <p className="text-gray-700 text-center">{person.rank}</p>
                 <p className="text-gray-500 text-center">{person.bureau}</p>
               </div>
             ))}
           </div>
-          {index < Object.keys(groupedPersonnel).length - 1 && <hr className="w-full border-t border-gray-300 my-8" />}
+          {index < Object.keys(groupedPersonnel).length - 1 && (
+            <hr className="w-full border-t border-gray-300 my-8" />
+          )}
         </div>
       ))}
     </div>
