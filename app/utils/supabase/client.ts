933c9c19cf4c4689ce4c963880cab024c5243fe5
Mehmetali ÖZTÜRK <mehmetali.ozturk@hotmail.com>
2025-02-28 23:50:47 +0300

    v0.8

--- app/utils/supabase/client.ts
+++ app/utils/supabase/client.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_ANON_KEY!;
+const supabaseUrl = "https://dweacuygywbnvsxxifch.supabase.co"
+const supabaseKey =  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3ZWFjdXlneXdibnZzeHhpZmNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAwNzkzODAsImV4cCI6MjA1NTY1NTM4MH0.Kr7Ny3XulzHM7LWSWvjXqQk9GQ5HfSiCXSdkIfPDCk0";
+
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
